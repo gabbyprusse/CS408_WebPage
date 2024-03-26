d875d2d398d34cad2cff098a4298a69798726935
gabbyprusse <gabbyprusse@u.boisestate.edu>
2024-03-25 18:09:06 -0600

    configured

--- AboutGabby.php
+++ AboutGabby.php
@@ -13,7 +13,7 @@
 <h1>Running from My Problems ...<img src="img.png" alt="cool pic"></h1>
 
 <ul class="MainPage">
-    <li><a href="Profile.html">Log In</a></li>
+    <li><a href="Profile.php">Log In</a></li>
     <li>
         <a href="https://www.roadrunnersports.com/?utm_source=google&utm_medium=cpc&utm_campaign=PMax%3A%20%28ROI%29%20Smart%20Shopping%202.0%20-%20Tier%201%20Shoes&utm_id=20990627255&utm_content=&utm_term=&gad_source=1&gclid=CjwKCAiAlcyuBhBnEiwAOGZ2S3QgAjEl_7vy-bZ1dn_P9NtmhkJ6fNrhglzZd3TSnSFCSzo0C1GvBoCFiYQAvD_BwE">Get Gear</a>
     </li>
@@ -24,8 +24,8 @@
         <a href="https://runningintheusa.com/classic/overview/">Upcoming Races </a>
     </li>
 
-    <li><a href="NewUser.html">Create Profile</a></li>
-    <li class="profileTitle"><a href="AboutGabby.html">About Gabby</a></li>
+    <li><a href="NewUser.php">Create Profile</a></li>
+    <li class="profileTitle"><a href="AboutGabby.php">About Gabby</a></li>
 </ul>
 
 <h1 class="benefits">Benefits of Running</h1>
