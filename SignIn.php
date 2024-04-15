21dddbee372c40c725e30e9b88644b359a17fb8c
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-15 15:08:39 -0600

    validation and sessions

--- SignIn.php
+++ SignIn.php
@@ -10,8 +10,6 @@
 <?php
     require_once "header.php";
     require_once "confiSession.php";
-    require_once "NUview.php";
-    require_once "SIview.php";
     require_once 'Dao.php';
     session_start();
 
