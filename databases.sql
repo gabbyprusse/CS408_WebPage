96950f2f73ffef02c8cb6fd6b8133515352c3896
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-16 22:11:27 -0600

    login/signout

--- databases.sql
+++ databases.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     id int(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(100) NOT NULL,
-    pwd VARCHAR(100) NOT NULL,
+    pwd VARCHAR(255) NOT NULL,
     goal int NOT NULL,
     PRIMARY KEY (id)
 );
