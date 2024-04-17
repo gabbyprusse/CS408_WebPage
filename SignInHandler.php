c6b3d64b59c2196f8264332182afbc53b8605310
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-16 21:20:14 -0600

    login/signout

--- SignInHandler.php
+++ SignInHandler.php
@@ -26,7 +26,6 @@ $dao = new Dao();
         $result = $dao->getUser($user);
         // checks if user and pwd are false
         if (!user_empty($user) && !pwd_empty($pwd)){
-
             if($result["pwd"] == null ||!validatePwd($pwd, $result["pwd"]) || !validateUsername($user, $result)) {
                 $errors["login_incorrect"] = "Incorrect login";
                 $_SESSION['authenticated'] = false;
