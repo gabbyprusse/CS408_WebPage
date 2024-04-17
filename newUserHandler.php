61f7b0ca6ddd20a4591c326bb2c93a109189c266
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-16 21:26:22 -0600

    login/signout

--- newUserHandler.php
+++ newUserHandler.php
@@ -8,7 +8,7 @@ session_start();
         if (0 == strlen($user)){
             $errors["emptyUser"] = "Fill in Username";
         }
-
+header('Location: Profile.php');
 
     // pwd reg expression : pwd needs a num in it
         $pattern = "/\d/";
