e8fa8b1d90a3eef57368d76cd52c9735809691d0
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-17 06:55:06 -0600

    login/signout

--- newUserHandler.php
+++ newUserHandler.php
@@ -11,7 +11,7 @@ session_start();
 
         // username reg expression : no white space
         $pattern = "/\s/";
-        if (!preg_match_all($pattern, $_POST['username'])) {
+        if (preg_match($pattern, $_POST['username']) == null) {
             $errors['invalidUsername'] = "Username should not contain whitespace";
         }
 
