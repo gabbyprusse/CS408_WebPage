96074cb7c95e07c44dbd6fab92c2d4201286354f
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-18 12:44:42 -0600

    repopulate

--- logout.php
+++ logout.php
@@ -1,8 +1,9 @@
 <?php
-if (session_id() == false) {
+//if (session_id() == false) {
     session_start();
-}
+//}
 $_SESSION['yes'] = false;
 session_destroy();
 header("Location: ../index.php");
 exit();
+?>
