d085f4607404b4a060bdf801687aedbafd40a6b6
Gabby Prusse <gabbyprusse@u.boisestate.edu>
2024-04-18 19:40:38 -0600

    nav

--- NewUser.php
+++ NewUser.php
@@ -10,13 +10,24 @@
     </head>
 
     <body>
+    <?php require_once "header.php";
+
+    $get_url = $_SERVER['REQUEST_URI'];
+    $curClass = str_replace(".php", "", $_SERVER['REQUEST_URI']);
+
+    ?>
+    <!-- HW7: Persistent nav  -->
+    <script>
+        let currentClass = document.querySelector('<?php echo $curClass?>');
+        if (currentClass) {
+            currentClass.style.color = 'mistyrose';
+        }
+    </script>
     <?php
-    require_once "header.php";
     require_once 'Dao.php';
     if (!session_id()) {
         session_start();
     }
-
 
         if (isset($_SESSION['errors_newuser'])) {
             foreach ($_SESSION['errors_newuser'] as $error) {
--- Profile.php
+++ Profile.php
@@ -18,7 +18,19 @@ if (!session_id()) {
 if (!isset($_SESSION['yes'])) {
     header('Location: SignIn.php');
 }
-require_once 'header.php';
+require_once "header.php";
+
+$get_url = $_SERVER['REQUEST_URI'];
+$curClass = str_replace(".php", "", $_SERVER['REQUEST_URI']);
+
+?>
+<!-- HW7: Persistent nav  -->
+<script>
+    let currentClass = document.querySelector('<?php echo $curClass?>');
+    if (currentClass) {
+        currentClass.style.color = 'mistyrose';
+    }
+</script>
 
 ///////??????????
 /// todo:
